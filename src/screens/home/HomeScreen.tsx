4ca71c19b6e10faae57a10b5d773ee1aaf652dcc
Thieme <thieme@hoogernet.nl>
2024-03-10 16:35:49 +0100

    Added floating map action

--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
+import { StyleSheet, View } from 'react-native'
 import MapView from 'react-native-maps'
 import { MapConfiguration } from '../../configuration/MapConfiguration'
+import FloatingMapAction from './components/FloatingMapAction'
 
 const HomeScreen = () => {
     const mapRef = React.useRef<MapView>(null)
@@ -15,18 +17,33 @@ const HomeScreen = () => {
     }, [])
 
     return (
-        <MapView
-            ref={mapRef}
-            provider="google"
-            customMapStyle={MapConfiguration.customMapStyle}
-            style={{ flex: 1 }}
-            region={region}
-            showsUserLocation
-            zoomControlEnabled
-            minZoomLevel={15.7}
-            maxZoomLevel={20}
-        />
+        <>
+            <MapView
+                ref={mapRef}
+                provider="google"
+                customMapStyle={MapConfiguration.customMapStyle}
+                style={{ flex: 1 }}
+                region={region}
+                showsUserLocation
+                zoomControlEnabled
+                minZoomLevel={15.7}
+                maxZoomLevel={20}
+            />
+            <View style={styles.floatingActionContainer}>
+                <FloatingMapAction />
+                <FloatingMapAction />
+                <FloatingMapAction />
+            </View>
+        </>
     )
 }
+
+const styles = StyleSheet.create({
+    floatingActionContainer: {
+        position: 'absolute',
+        bottom: 50,
+        right: 4
+    }
+})
 
 export default HomeScreen
---
+++ src/screens/home/components/FloatingMapAction.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+
+interface FloatingMapActionProps {
+    onPress?: () => void
+}
+
+const FloatingMapAction = (props: FloatingMapActionProps) => {
+    return (
+        <TouchableOpacity onPress={props.onPress}>
+            <View style={styles.container}>
+                <Text>+</Text>
+            </View>
+        </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 10,
+        width: 50,
+        height: 50,
+        backgroundColor: 'red',
+        padding: 5,
+        margin: 2,
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+})
+
+export default FloatingMapAction
