5aff7d3f54c06d7b5efe3e3b04fbc480f11b5394
Thieme <thieme@hoogernet.nl>
2024-03-15 12:04:47 +0100

    Removed create notification from settings

--- src/screens/settings/SettingScreen.tsx
+++ src/screens/settings/SettingScreen.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { Button, Text, View } from 'react-native'
 
 const SettingScreen = ({ navigation }: any) => {
-  return (
-    <View>
-      <Text>SettingScreen</Text>
-      <Button title="Go to Notification" onPress={() => navigation.push('Notification')} />
-      <Button title="Create Notification" onPress={() => navigation.push('NotificationCreate')} />
-    </View>
-  )
+    return (
+        <View>
+            <Text>SettingScreen</Text>
+            <Button
+                title="Go to Notification"
+                onPress={() => navigation.push('Notification')}
+            />
+        </View>
+    )
 }
 
 export default SettingScreen
