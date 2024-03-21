f94fed45077fecfe2fa56c993198c27aa5ab62e2
Mauro <mr.weyers@student.avans.nl>
2024-03-21 18:06:16 +0100

    Added icons to bottom tab navigator

--- src/navigation/BottomTabNavigator.tsx
+++ src/navigation/BottomTabNavigator.tsx
@@ -1,16 +1,28 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { HomeStackNavigator } from './HomeStackNavigator'
 import { SettingStackNavigator } from './SettingStackNavigator'
+import Ionicons from '@expo/vector-icons/build/Ionicons'
 
 const BottomTab = createBottomTabNavigator()
 
 export const BottomTabNavigator = () => {
     return (
         <BottomTab.Navigator screenOptions={{ headerShown: false }}>
-            <BottomTab.Screen name="HomeStack" component={HomeStackNavigator} />
+            <BottomTab.Screen 
+                name="HomeStack"
+                component={HomeStackNavigator} 
+                options={{
+                    title: 'Home',
+                    tabBarIcon: ({color}) => <Ionicons name="home" size={32} color={color} />
+                }}
+            />
             <BottomTab.Screen
                 name="SettingsStack"
                 component={SettingStackNavigator}
+                options={{
+                    title: 'Settings',
+                    tabBarIcon: ({color}) => <Ionicons name="cog" size={32} color={color} />
+                }}
             />
         </BottomTab.Navigator>
     )
