3cfc10af9e8f27965ca75c6bc029d06cc2975781
Mauro <mr.weyers@student.avans.nl>
2024-03-26 15:24:11 +0100

    Fixed bug not being able to open facility that was just closed

--- src/screens/home/facility/FacilityDetailBottomSheet.tsx
+++ src/screens/home/facility/FacilityDetailBottomSheet.tsx
@@ -1,8 +1,9 @@
 import BottomSheet from '@gorhom/bottom-sheet'
 import React, { useEffect } from 'react'
 import { Button, Text, View } from 'react-native'
-import { useSelector } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { RootState } from '../../../redux/store'
+import { bottomSheetSlice } from '../../../redux/reducers/bottomSheetReducer'
 
 const FacilityDetailBottomSheet = ({ navigation }: any) => {
     const facility = useSelector(
@@ -10,33 +11,42 @@ const FacilityDetailBottomSheet = ({ navigation }: any) => {
     )
     const bottomSheetRef = React.useRef<BottomSheet>(null)
 
+    const dispatch = useDispatch()
+
     useEffect(() => {
-        bottomSheetRef.current?.expand()
+        if (facility) {
+            bottomSheetRef.current?.expand()
+        }
     }, [facility])
 
     return (
-        <BottomSheet index={-1} ref={bottomSheetRef} snapPoints={['80%', '40%']} enablePanDownToClose>
+        <BottomSheet index={-1} ref={bottomSheetRef} snapPoints={['80%', '40%']} enablePanDownToClose
+            onClose={
+                () => {
+                    dispatch(bottomSheetSlice.actions.closeBottomSheet());
+                }
+            }>
             {facility && (
-            <View
-                style={{
-                    flex: 1,
-                }}
-            >
-                <Text style={{ marginHorizontal: 20, fontWeight: 'bold', fontSize: 18 }}>{facility.name}</Text>
-                <Text style={{ marginHorizontal: 20}}>{facility.type}</Text>
-                <Text style={{ marginLeft: 20, fontWeight: 'bold' , marginTop: 10 }}>
-                    Beschrijving
-                </Text>
-                <Text style={{ marginHorizontal: 20 }}>{facility.description}</Text>
-                <View style={{ marginHorizontal: 20, marginVertical: 15 }}>
-                    <Button
-                        title="Faciliteitsgegevens wijzigen"
-                        onPress={() =>
-                            navigation.navigate('UpsertFacility', { facility })
-                        }
-                    />
+                <View
+                    style={{
+                        flex: 1,
+                    }}
+                >
+                    <Text style={{ marginHorizontal: 20, fontWeight: 'bold', fontSize: 18 }}>{facility.name}</Text>
+                    <Text style={{ marginHorizontal: 20 }}>{facility.type}</Text>
+                    <Text style={{ marginLeft: 20, fontWeight: 'bold', marginTop: 10 }}>
+                        Beschrijving
+                    </Text>
+                    <Text style={{ marginHorizontal: 20 }}>{facility.description}</Text>
+                    <View style={{ marginHorizontal: 20, marginVertical: 15 }}>
+                        <Button
+                            title="Faciliteitsgegevens wijzigen"
+                            onPress={() =>
+                                navigation.navigate('UpsertFacility', { facility })
+                            }
+                        />
+                    </View>
                 </View>
-            </View>
             )}
         </BottomSheet>
     )
