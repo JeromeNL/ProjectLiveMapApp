a08260647ec5004efb3a10de68377a2f51fef96d
Mauro <mr.weyers@student.avans.nl>
2024-03-26 02:15:04 +0100

    Reformatting of detail page

--- src/screens/home/facility/FacilityDetail.tsx
+++ src/screens/home/facility/FacilityDetail.tsx
@@ -2,18 +2,23 @@ import React from 'react';
 import { View, Text, Image, Button } from 'react-native';
 import { Facility } from '../../../model/Facility';
 
-const FacilityDetailView = ({ route,  navigation }: any) => {
+const FacilityDetailView = ({ route, navigation }: any) => {
     const facility: Partial<Facility> = route.params?.facility
 
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Naam: {facility.name}</Text>
-            <Text>Beschrijving: {facility.description}</Text>
-            <Text>Type: {facility.type}</Text>
-            <Button
-                title="Faciliteitsgegevens wijzigen"
-                onPress={() => navigation.navigate('UpsertFacility', { facility })} // Use `navigation` instead of `nav` if it's a prop
-            />
+            <Text style={{margin: 5, fontWeight: 'bold'}}>Naam</Text>
+            <Text style={{margin: 5}}>{facility.name}</Text>
+            <Text style={{margin: 5, fontWeight: 'bold'}}>Type</Text>
+            <Text style={{margin: 5}}>{facility.type}</Text>
+            <Text style={{margin: 5, fontWeight: 'bold'}}>Beschrijving</Text>
+            <Text style={{margin: 5}}>{facility.description}</Text>
+            <View style={{margin: 10}}>
+                <Button
+                    title="Faciliteitsgegevens wijzigen"
+                    onPress={() => navigation.navigate('UpsertFacility', { facility })}
+                />
+            </View>
         </View>
     );
 };
