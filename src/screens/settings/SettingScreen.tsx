6e57765967c3e6fd1a8a047a579efcc27afeac67
ImkeLooman <isa.looman@student.avans.nl>
2024-04-20 21:19:56 +0200

    Added space between buttons in settingsscreen

--- src/screens/settings/SettingScreen.tsx
+++ src/screens/settings/SettingScreen.tsx
@@ -17,6 +17,7 @@ const SettingScreen = ({ navigation }: any) => {
                     title="Notificaties"
                     onPress={() => navigation.push('Notification')}
                 />
+                <View style={styles.spaceBetweenContainer} />
                 <Button
                     title="Meldingen"
                     onPress={() => navigation.push('ReportsList')}
@@ -39,7 +40,10 @@ const styles = StyleSheet.create({
     centeredContainer: {
         flex: 1,
         justifyContent: 'center',
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    spaceBetweenContainer: {
+        height: 20,
     },
     bottomContainer: {
         marginBottom: 20
